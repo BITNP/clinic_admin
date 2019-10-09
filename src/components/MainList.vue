7bb199970297f73309ae0a03d730708e5d22d938
FKYnJYQ <loveress01@gmail.com>
2019-10-09 14:00:46 +0800

    refactor(mainlist): 删除无用的注释代码

--- src/components/MainList.vue
+++ src/components/MainList.vue
@@ -2,6 +2,7 @@
   <v-container fluid fill-height>
     <v-row justify="center">
       <v-col cols="12" v-if="filterBar">
+        <!-- 这里有用来过滤的几个工具 -->
         <v-row>
           <v-col cols="4" md="6" sm="12">
             <v-text-field
@@ -41,6 +42,9 @@
               chips
             ></v-combobox>
           </v-col>
+          <v-col cols="4" md="6" sm="12">
+            <NewRecord></NewRecord>
+          </v-col>
         </v-row>
       </v-col>
       <v-col cols="12">
@@ -53,7 +57,9 @@
                 </v-col>
               </v-row>
               <template v-slot:actions>
-                <v-icon @click="filterBar = !filterBar" color="primary">mdi-apps</v-icon>
+                <v-icon @click="filterBar = !filterBar" color="primary"
+                  >mdi-apps</v-icon
+                >
               </template>
             </v-expansion-panel-header>
           </v-expansion-panel>
@@ -77,7 +83,9 @@
                       'brown--text': i == 5
                     }"
                     class="margin-fix text-truncate"
-                  >{{ item || "无" }}</div>
+                  >
+                    {{ item || "无" }}
+                  </div>
                 </v-col>
               </v-row>
             </v-expansion-panel-header>
@@ -88,7 +96,9 @@
                 <v-card-text>
                   <v-row>
                     <!-- TEXT显示的内容 -->
-                    <template v-for="(text_render_item, ii) in TEXT_RENDER_LIST">
+                    <template
+                      v-for="(text_render_item, ii) in TEXT_RENDER_LIST"
+                    >
                       <v-col
                         cols="3"
                         pa-0
@@ -101,19 +111,20 @@
                               large
                               left
                               :color="ICON_COLOR[text_render_item]"
-                            >{{ ICON_LIST[text_render_item] }}</v-icon>
-                            <span
-                              class="title font-weight-light"
-                            >{{ KEY_TRANSLATION[text_render_item] }}</span>
+                              >{{ ICON_LIST[text_render_item] }}</v-icon
+                            >
+                            <span class="title font-weight-light">
+                              {{ KEY_TRANSLATION[text_render_item] }}
+                            </span>
                           </v-card-title>
                           <v-card-text>
                             <p>
                               {{
-                              text_render_item != "worker"
-                              ? r[text_render_item] || "无"
-                              : r[text_render_item]
-                              ? url2User(r[text_render_item])["realname"]
-                              : ""
+                                text_render_item != "worker"
+                                  ? r[text_render_item] || "无"
+                                  : r[text_render_item]
+                                  ? url2User(r[text_render_item])["realname"]
+                                  : ""
                               }}
                             </p>
                           </v-card-text>
@@ -121,7 +132,9 @@
                       </v-col>
                     </template>
                     <!-- INPUT显示的内容 -->
-                    <template v-for="(input_render_item, ii) in INPUT_RENDER_LIST">
+                    <template
+                      v-for="(input_render_item, ii) in INPUT_RENDER_LIST"
+                    >
                       <v-col
                         cols="3"
                         pa-0
@@ -134,10 +147,11 @@
                               large
                               left
                               :color="ICON_COLOR[input_render_item]"
-                            >{{ ICON_LIST[input_render_item] }}</v-icon>
-                            <span
-                              class="title font-weight-light"
-                            >{{ KEY_TRANSLATION[input_render_item] }}</span>
+                              >{{ ICON_LIST[input_render_item] }}</v-icon
+                            >
+                            <span class="title font-weight-light">
+                              {{ KEY_TRANSLATION[input_render_item] }}
+                            </span>
                           </v-card-title>
                           <v-card-text>
                             <v-text-field
@@ -151,15 +165,19 @@
                       </v-col>
                     </template>
                     <!-- select显示的内容 -->
-                    <v-col cols="3" pa-0 v-if="SELECT_LIST['worker'].includes(r.status)">
+                    <v-col
+                      cols="3"
+                      pa-0
+                      v-if="SELECT_LIST['worker'].includes(r.status)"
+                    >
                       <v-card hover height="100%">
                         <v-card-title>
-                          <v-icon large left :color="ICON_COLOR['worker']">
-                            {{
+                          <v-icon large left :color="ICON_COLOR['worker']">{{
                             ICON_LIST["worker"]
-                            }}
-                          </v-icon>
-                          <span class="title font-weight-light">{{ KEY_TRANSLATION["worker"] }}</span>
+                          }}</v-icon>
+                          <span class="title font-weight-light">
+                            {{ KEY_TRANSLATION["worker"] }}
+                          </span>
                         </v-card-title>
                         <v-card-text>
                           <!-- :hint="" -->
@@ -188,7 +206,9 @@
                         :color="r.is_taken ? 'success' : 'warning'"
                       >
                         {{ r.is_taken ? "已取走" : "未取走" }}
-                        <v-icon dark>mdi-{{ r.is_taken ? "check" : "close" }}</v-icon>
+                        <v-icon dark
+                          >mdi-{{ r.is_taken ? "check" : "close" }}</v-icon
+                        >
                       </v-btn>
                     </v-col>
                     <!-- 发送短信通知 按钮 -->
@@ -230,19 +250,34 @@
           </v-expansion-panel>
         </v-expansion-panels>
       </v-col>
+
       <v-col cols="3" class="text-xs-center mt-1">
-        <v-btn text block rounded dark v-if="hasNext" color="teal" @click="next(hasNext)">更多</v-btn>
+        <v-btn
+          text
+          block
+          rounded
+          dark
+          v-if="hasNext"
+          color="teal"
+          @click="next(hasNext)"
+          >更多</v-btn
+        >
       </v-col>
+      <!-- FAB 浮动的button -->
+      <FlowActionButton></FlowActionButton>
     </v-row>
   </v-container>
 </template>
 
 <script>
 import Record from "@/components/Record";
-
+import NewRecord from "@/components/NewRecord";
+import FlowActionButton from "@/components/FlowActionButton";
 export default {
   components: {
-    Record
+    Record,
+    NewRecord,
+    FlowActionButton
   },
   data: () => ({
     filterBar: false,
@@ -294,145 +329,8 @@ export default {
       4: [5],
       5: [7, 8, 6]
     }
-    // INPUT_HELPER_TEXT: {
-    //   worker: "如不填写则为默认为当前登录用户",
-    //   worker_description: "简要描述情况",
-    //   method: "简要描述处理方法",
-    //   reject_reason: "只有驳回时才会提交",
-    //   model: "更专业的记录"
-    // },
-    // ICON_LIST: {
-    //   description: "mdi-text",
-    //   model: "mdi-laptop",
-    //   reject_reason: "mdi-alert-octagon",
-    //   password: "mdi-key",
-    //   worker: "mdi-hammer",
-    //   worker_description: "mdi-comment",
-    //   method: "mdi-function"
-    // },
-    // ICON_COLOR: {
-    //   description: "indigo",
-    //   model: "deep-purple",
-    //   reject_reason: "teal",
-    //   password: "lime",
-    //   worker: "brown",
-    //   worker_description: "cyan",
-    //   method: "amber"
-    // },
-    // DISPLAY_LIST: {
-    //   description: [0, 1, 2, 3, 4, 5, 6, 7],
-    //   model: [6, 7, 8],
-    //   reject_reason: [3],
-    //   password: [5, 6, 7, 8],
-    //   worker: [6, 7, 8],
-    //   worker_description: [6, 7, 8],
-    //   method: [6, 7, 8]
-    // },
-    // INPUT_LIST: {
-    //   reject_reason: [1],
-    //   model: [5],
-    //   worker_description: [5],
-    //   method: [5]
-    // },
-    // SELECT_LIST: {
-    //   worker: [5]
-    // }
-    // KEY_TRANSLATION: {
-    //   description: "问题描述",
-    //   model: "电脑型号",
-    //   reject_reason: "拒绝理由",
-    //   password: "电脑开机密码",
-    //   worker: "维修负责人",
-    //   worker_description: "维修人员问题描述",
-    //   method: "处理方法"
-    // },
-    // TEXT_RENDER_LIST: [
-    //   "description",
-    //   "model",
-    //   "reject_reason",
-    //   "password",
-    //   "worker",
-    //   "worker_description",
-    //   "method"
-    // ],
-    // BUTTON_LIST: [
-    //   [{ label: "已到诊所", color: "primary", icon: "check" }],
-    //   [
-    //     { label: "驳回预约", color: "error", icon: "cancel" },
-    //     { label: "确认受理", color: "primary", icon: "check" }
-    //   ],
-    //   [{ label: "已到诊所", color: "primary", icon: "check" }],
-    //   [],
-    //   [{ label: "已到诊所", color: "primary", icon: "check" }],
-    //   [
-    //     { label: "建议返厂", color: "purple", icon: "wrench" },
-    //     { label: "扔给明天", color: "info", icon: "clock-outline" },
-    //     { label: "已解决问题", color: "success", icon: "check-all" }
-    //   ],
-    //   [],
-    //   [],
-    //   []
-    // ],
-    // INPUT_RENDER_LIST: [
-    //   "model",
-    //   "worker_description",
-    //   "method",
-    //   "reject_reason"
-    // ],
-    // SELECT_RENDER_LIST: ["worker"]
   }),
   methods: {
-    // updateRecord(
-    //   url,
-    //   to_status,
-    //   fields = [],
-    //   record = {},
-    //   changeSomeThing = null
-    // ) {
-    //   this.$store.commit("loading");
-
-    //   if (to_status == 8) {
-    //     this.$http
-    //       .post("/api/records/", {
-    //         ...record,
-    //         status: 0,
-    //         appointment_time: new Date(
-    //           new Date().getTime + 24 * 60 * 60 * 1000
-    //         ).toLocaleDateString(),
-    //         worker: null,
-    //         arrive_time: null
-    //       })
-    //       .then(response => {
-    //         this.$store.commit("popSuccess", "成功创建新工单");
-    //         // 需要用户手动更新视图
-    //       })
-    //       .catch(error => {
-    //         console.log(error);
-    //         this.$store.commit("popError", "未能成功创建工单");
-    //         return;
-    //       })
-    //       .finally(() => this.$store.commit("loaded"));
-    //   }
-    //   if (record.status != 5 && to_status == 5) {
-    //     // 已到
-    //     record.arrive_time = new Date().toISOString();
-    //   }
-    //   if (changeSomeThing) {
-    //     record.is_taken = !record.is_taken;
-    //   }
-    //   this.$http
-    //     .put(url, { ...record, status: to_status })
-    //     .then(response => {
-    //       record.status = to_status;
-    //       this.$store.commit("popSuccess", "更新成功");
-    //     })
-    //     .catch(error => {
-    //       console.log(error);
-    //       record.status = to_status;
-    //       this.$store.commit("popError", "更新失败");
-    //     })
-    //     .finally(() => this.$store.commit("loaded"));
-    // },
     next(url) {
       this.$store.commit("getData", url);
     },
@@ -456,14 +354,6 @@ export default {
       if (!isNaN(d.getTime())) return d.toLocaleString();
       return "";
     },
-    // sendSMS(url) {
-    //   this.$http
-    //     .get(url + "call_user_back")
-    //     .then(response => {
-    //       this.$store.commit("popSuccess", response.data.detail);
-    //     })
-    //     .catch(e => this.$store.commit("popError", e));
-    // },
     filterSuccess(success) {
       // 成功与否
       if (success) {
